69235c3f75cd8cc38dbe4c2fbf22660ddf6c294f
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2017-05-31 22:38:55 -0300

    adicionando novo artigo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -25,6 +26,10 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Config bodyParser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 // index route
 app.get('/', function(req, res) {
   Article.find({}, function(err, articles){
@@ -39,10 +44,26 @@ app.get('/', function(req, res) {
   });
 });
 
-// add article route
+// new article route
 app.get('/articles/add', function(req, res) {
   res.render('add', {
     title: 'Add Articles'
+  });
+});
+
+// add article route
+app.post('/articles/add', function(req, res) {
+  let article = new Article();
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  article.save(function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
   });
 });
 
